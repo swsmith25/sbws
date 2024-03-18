172c62afc76dafd653278619a31631ecd4155842
Bo Smith <swsmith25@utexas.edu>
2024-03-18 01:30:03 -0500

    issue: i think there is an issue w/ github pages? it only shows old index html. seeing if pushing fixes

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.css'
+import NavBar from './components/NavBar'
+
+class App extends Component {
+    render(){
+        return(
+            
+        )
+    }
+}
