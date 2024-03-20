728bff361c4551cf04ce70b053e531440401d209
Bo Smith <swsmith25@utexas.edu>
2024-03-19 20:09:20 -0500

    Updates

---
+++ precache-manifest.3adf92fdf70b3473fc89a88a1fef2c9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8715d65ed452feed148211eee9dd5f",
+    "url": "/swsmith25/sbws/index.html"
+  },
+  {
+    "revision": "c9588c5d00e90e2df524",
+    "url": "/swsmith25/sbws/static/css/main.cd10a404.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/swsmith25/sbws/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/swsmith25/sbws/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c9588c5d00e90e2df524",
+    "url": "/swsmith25/sbws/static/js/main.c52684ed.chunk.js"
+  },
+  {
+    "revision": "b5ace3e1cb1d64aef2d2",
+    "url": "/swsmith25/sbws/static/js/runtime~main.a303eb0d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/swsmith25/sbws/static/media/logo.06e73328.svg"
+  }
+]);
